2a0abdc2df91f03027328da97421bf55ae96d58a
VazelevRV <vazelevrv@krok.edu.ua>
2021-04-28 20:31:32 +0300

    fixed templates rendering; started work designing pages

--- src/django_food_main/forms.py
+++ src/django_food_main/forms.py
@@ -4,14 +4,14 @@ from .models import Reservation
 
 class ReservationForm(forms.ModelForm):
     user_name = forms.CharField(max_length=50, widget=forms.TextInput(attrs={
-        'type': 'text', 'id': 'name', 'class': 'form-control', 'placeholder': 'Name', 'required': 'required'}))
+        'type': 'text', 'id': 'name', 'class': 'form-control', 'placeholder': 'Ім\'я', 'required': 'required'}))
     user_email = forms.EmailField(widget=forms.TextInput(attrs={'type': 'email', 'id': 'email',
                                                                 'class': 'form-control', 'placeholder': 'example@gmail.com',
                                                                 'required': 'required'}))
     message = forms.CharField(max_length=200,
                                    widget=forms.Textarea(
                                        attrs={'name': 'message', 'id': 'message', 'class': 'form-control',
-                                              'rows': '4', 'placeholder': 'your message',
+                                              'rows': '4', 'placeholder': 'Повідомлення',
                                               'required': 'required'}))
 
     class Meta:
